e2f5131614bad1e638587f90f4bb640f437c75b6
sanidavidanagama <sanidavidanagama@gmail.com>
2026-06-15 19:12:07 +0530

    refactor: update ui elements /assets

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+server: {
+  host: true
+}
 
 export default defineConfig({
   plugins: [
