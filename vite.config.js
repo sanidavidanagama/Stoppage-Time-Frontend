06a4e9d6380a7b8c2dee5438e3ee0062ac3914a6
sanidavidanagama <sanidavidanagama@gmail.com>
2026-06-15 22:59:53 +0530

    fix: update the vite config server typo issue

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-server: {
-  host: true
-}
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: true
+  }
 })
