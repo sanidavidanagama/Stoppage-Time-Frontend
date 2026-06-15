f25d5f4b7b15706ebac0cc99db8a6ac87c2927a3
sanidavidanagama <sanidavidanagama@gmail.com>
2026-06-16 01:48:23 +0530

    feat: replace VITE url with config api url

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
+const API_URL = window.configs?.apiUrl || import.meta.env.VITE_API_URL
+
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API_URL,
 })
 
 client.interceptors.request.use((config) => {
