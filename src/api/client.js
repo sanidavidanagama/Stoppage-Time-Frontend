b70718fe6513b3972c15aa1632a321a1c84a3637
sanidavidanagama <sanidavidanagama@gmail.com>
2026-06-18 12:05:48 +0530

    feat: add the backend api

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-const API_URL = window.configs?.apiUrl || import.meta.env.VITE_API_URL
+const API_URL = 'https://17eeed1a-8826-4d97-86c4-ec780162a899-dev.e1-us-east-azure.choreoapis.dev/stoppage-time/backend/v1.0'
 
 const client = axios.create({
   baseURL: API_URL,
